38fde3fc22efbf477a21f8b89daeebd300a2b60d
allnrvao <aracuna@uamv.edu.ni>
2026-05-20 21:13:12 -0600

    Feat: Se crea el proyecto

---
+++ registroVuelos-API/src/main/java/ni/edu/uam/registroVuelos_API/RegistroVuelosApiApplication.java
@@ -0,0 +1,13 @@
+package ni.edu.uam.registroVuelos_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroVuelosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroVuelosApiApplication.class, args);
+	}
+
+}
